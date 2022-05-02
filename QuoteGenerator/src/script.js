7535a3ded6d87794fb68889dff2520624b1162f4
Matt <31101519+Kronus8@users.noreply.github.com>
2022-05-02 21:12:19 +0100

    Update script.js

--- QuoteGenerator/src/script.js
+++ QuoteGenerator/src/script.js
@@ -5,7 +5,7 @@ const newQuoteButton = document.querySelector("button");
 function randomQuote () {
     newQuoteButton.innerText = "Loading Quote..."
     // https://github.com/lukePeavey/quotable#get-random-quote
-    fetch('http://api.quotable.io/random').then(response => response.json()).then(result => {
+    fetch('https://api.quotable.io/random').then(response => response.json()).then(result => {
         quoteElement.innerHTML = result.content;
         authorElement.innerHTML = result.author;
         newQuoteButton.innerText = 'New Quote';
