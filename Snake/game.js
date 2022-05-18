bdd86a5a04fd755a2ef963815eb7a97f44b8c245
Matt <31101519+Kronus8@users.noreply.github.com>
2022-05-18 22:12:27 +0100

    Update game.js

--- Snake/game.js
+++ Snake/game.js
@@ -147,7 +147,7 @@ function checkAppleCollision() {
 }
 
 // Key event listener.
-document.body.addEventListener('keydown', function(e) {
+window.addEventListener('keydown', function(e) {
     // Up
     if (e.key === 'W') {
         if (yVelocity == 1)
